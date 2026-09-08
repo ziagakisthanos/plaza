21da8515e6b3227ea5703d344fbb91bba09bfd1d
ziagakisthanos <ziagakisthanos@gmail.com>
2026-09-08 19:58:55 +0300

    media service module scaffold

---
+++ media-service/src/main/java/platform/zone01/mediaservice/MediaServiceApplication.java
@@ -0,0 +1,13 @@
+package platform.zone01.mediaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediaServiceApplication.class, args);
+    }
+
+}
---
+++ media-service/src/test/java/platform/zone01/mediaservice/MediaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package platform.zone01.mediaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
